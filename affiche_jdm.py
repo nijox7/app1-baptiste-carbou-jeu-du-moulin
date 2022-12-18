ef1902b420b44b2b172a9f55da3cdeaf81b513e5
Clément <113636309+nijox7@users.noreply.github.com>
2022-12-18 17:02:55 +0100

    Update affiche_jdm.py

--- affiche_jdm.py
+++ affiche_jdm.py
@@ -2,18 +2,8 @@ from fltk import*
 from Case import*
 
 largeur, hauteur = 900, 900
-moyenne = (largeur + hauteur) / 2 #Moyenne qui permet d'ajuster la taille du quadrillage
+moyenne = (largeur + hauteur) / 1.5 #Moyenne qui permet d'ajuster la taille du quadrillage
 a = 0.25 * moyenne #Distance du centre au bord de la grille
-
-
-def cree_plateau():
-    ''' Renvoie un plateau vide de la variante à 9 jetons '''
-    t = [[Case(i, j, "X") for j in range(7)] for i in range(7)]
-    for i in range(7):
-        t[i][i], t[i][6-i] = Case(i, i, ""), Case(i, 6-i, "")
-        t[3][i], t[i][3] = Case(3, i, ""), Case(i, 3, "")
-    t[3][3] = Case(3, 3, "Centre")
-    return t
 
 
 def dessine_plateau():
@@ -37,7 +27,7 @@ def dessine_plateau():
           epaisseur=5)
 
 
-def dessine_pion(plateau, case):
+def dessine_pion(case):
     ''' Dessine un pion sur une case '''
     nom = str(case.x)+str(case.y)
     cercle(largeur/2 - a + case.y * (a/3),  hauteur/2 - a + case.x * (a/3),
@@ -45,7 +35,7 @@ def dessine_pion(plateau, case):
            tag="pion"+str(case.x)+str(case.y), epaisseur=2)
 
 
-def efface_pion(plateau, case):
+def efface_pion(case):
     ''' Efface le pion d'une casse '''
     efface("pion" + str(case.x) + str(case.y))
 
@@ -65,9 +55,9 @@ def donne_case(plateau):
                 return plateau[i][j]
 
 
-def affiche_liste_case(plateau, liste):
+def dessine_liste_cases(liste):
     ''' Affiche une liste de case '''
     for case in liste:
-        cercle(largeur/2 - a + case.x * (a/3), hauteur/2 - a + case.y * (a/3),
+        cercle(largeur/2 - a + case.y * (a/3), hauteur/2 - a + case.x * (a/3),
                moyenne * 0.02, couleur="red", tag="case_possible")
 
