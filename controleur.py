f10ef750cd06ef21cf225adba9f7c6beb426a60a
Clément <113636309+nijox7@users.noreply.github.com>
2023-01-02 14:35:40 +0100

    Update controleur.py

--- controleur.py
+++ controleur.py
@@ -1,22 +1,40 @@
 from vue import*
+from modele import*
+
+### INITIALISATION DU MODELE ###
+
+#Création du plateau
+plateau = cree_plateau()
+
+#Définition des voisines et des alignements possibles de chaque case
+for i in range (7):
+    for j in range(7):
+        case = plateau[i][j]
+        if case.couleur == "":
+            case.voisines = vois_hor(plateau, i, j) + vois_vert(plateau, i, j)
+            case.alignements[0] = ligne_aligne(plateau, i, j)
+            case.alignements[1] = col_aligne(plateau, i, j)
+            case.alignements[2], case.alignements[3] = diag_aligne(plateau, i, j)
 
 cree_fenetre(largeur, hauteur)
-#image(largeur/2, hauteur/2, "fond1.gif", ancrage="center")
-dessine_plateau()
-affiche_case()
+image(largeur/2, hauteur/2, "fond1.gif", ancrage="center")
+nb_jetons = 9
 j1 = "black"
 j2 = "white"
-joueur = "black"
-compteur = {j1: nb_jetons, j2: nb_jetons}
-running = True
+joueur = j1
+nb_pion_main = {j1: nb_jetons, j2: nb_jetons}
+nb_pion_plat = {j1: 0, j2: 0}
+dessine_plateau_9j()
+affiche_case(plateau)
 phase1 = True
 phase2 = False
-phase3_j1 = False
-phase3_j2 = False
+phase3 = {j1: False, j2: False}
 deplacement = False
+saut = False
 retire = False
+game = True
 
-while True: 
+while game: 
     ev = attend_ev()
     tev = type_ev(ev)
 
@@ -25,90 +43,105 @@ while True:
         break 
 
     if tev == "ClicGauche":
-        
-        #Le joueur retire un pion après la formation d'un moulin
-        if retire:
-            print("dans retire")
-            case = donne_case()
-            if case and case.couleur == joueur_adv(joueur) and case.moulin == False:
-                case.retire()
-                efface_pion(case)
-                retire = False
-                joueur = joueur_adv(joueur)
+        case = donne_case(plateau)
+        if case:
+            #Le joueur retire un pion après la formation d'un moulin
+            if retire:
+                print("dans retire")
+                #vérifie que le joueur peut retirer le pion
+                if case.couleur == joueur_adv(joueur, j1, j2):
+                    if (case.moulin == False or phase3[joueur_adv(joueur, j1, j2)]):
+                        case.retire()
+                        nb_pion_plat[joueur_adv(joueur, j1, j2)] -= 1
+                        efface_pion(case)
+                        retire = False
+                        joueur = joueur_adv(joueur, j1, j2)
+                        if phase2:
+                            if nb_pion_plat[joueur] == 3:
+                                phase3[joueur] = True
+                            elif nb_pion_plat[joueur] < 3:
+                                game = False
+                                break
 
-        #Le joueur doit placer un pion
-        elif phase1:
-            print("dans phase 1")
-            #Renvoie si la souris se trouve sur une case du plateau
-            case = donne_case()
-            if case and case.est_vide():
-                case.place(joueur)
-                dessine_pion(case)
-                compteur[joueur] -= 1
-                #Si un moulin est formé, le joueur doit retirer un pion
-                liste_aligne = detect_moulin(case)
-                if liste_aligne != []:
-                    case.moulin = True
-                    dessine_moulin(case)
-                    for alignement in liste_aligne:
-                        for case_aligne in alignement:
-                            case_aligne.moulin = True
-                            dessine_moulin(case_aligne)
-                    retire = True
+            #Le joueur doit placer un pion
+            elif phase1:
+                print("dans phase1")
+                #Renvoie si la souris se trouve sur une case du plateau
+                if case.est_vide():
+                    case.place(joueur)
+                    dessine_pion(case)
+                    nb_pion_main[joueur] -= 1
+                    nb_pion_plat[joueur] +=1
+                    #Si un moulin est formé, le joueur doit retirer un pion
+                    liste_aligne = detect_moulin(case)
+                    if liste_aligne != []:
+                        case.moulin = True
+                        dessine_moulin(case)
+                        for alignement in liste_aligne:
+                            for case_aligne in alignement:
+                                case_aligne.moulin = True
+                                dessine_moulin(case_aligne)
+                        retire = True
+                    else:
+                        joueur = joueur_adv(joueur, j1, j2)
+                #Détecte quand chaque joueur a joué la totalité de ses pions
+                if phase1 and nb_pion_main[j1] == 0 and nb_pion_main[j2] == 0:
+                    phase1 = False
+                    phase2 = True
+
+            #Le joueur choisit où déplacer son pion
+            elif deplacement or saut:
+                print("en déplacement ou saut")
+                #Le joueur sélectionne sa destination
+                dest = case
+                if (saut and case.couleur == "") or peut_deplacer(depart, dest):
+                    liste_aligne_dep = detect_moulin(depart) #liste des cases alignées à la case de départ
+                    deplace(depart, dest, joueur)
+                    liste_aligne_dest = detect_moulin(dest) #liste des cases alignées à la case de destination
+                    efface_pion(depart)
+                    efface("case_possible")
+                    dessine_pion(dest)
+                    deplacement = False
+                    saut = False
+                    #Supprime l'ancien marquage
+                    if liste_aligne_dep != []:
+                        efface_moulin(depart)
+                        for alignement in liste_aligne_dep:
+                            for case_aligne in alignement:
+                                if detect_moulin(case_aligne) == []:
+                                    case_aligne.moulin = False #La case n'est plus alignée
+                                    efface_moulin(case_aligne)
+                    #Marque les cases si alignement
+                    if liste_aligne_dest != []:
+                        retire = True
+                        dest.moulin = True
+                        dessine_moulin(dest)
+                        for alignement in liste_aligne_dest:
+                            for case_aligne in alignement:
+                                case_aligne.moulin = True #Marque la case comme alignée
+                                dessine_moulin(case_aligne)
+                    else:
+                        joueur = joueur_adv(joueur, j1, j2)
                 else:
-                    joueur = joueur_adv(joueur)
-            #Détecte quand chaque joueur a joué la totalité de ses pions
-            if phase1 and compteur["black"] == 0 and compteur["white"] == 0:
-                phase1 = False
-                phase2 = True
+                    deplacement = False
+                    saut = False
+                efface("case_possible")           
 
-        #Le joueur choisit où déplacer son pion
-        elif deplacement:
-            print("se déplace")
-            #Le joueur sélectionne sa destination
-            dest = donne_case()
-            if dest and peut_deplacer(depart, dest):
-                liste_aligne_dep = detect_moulin(depart)
-                deplace(depart, dest, joueur)
-                liste_aligne_dest = detect_moulin(dest)
-                efface_pion(depart)
-                efface("case_possible")
-                dessine_pion(dest)
-                deplacement = False
-                #Supprime l'ancien marquage
-                if liste_aligne_dep != []:
-                    efface_moulin(depart)
-                    for alignement in liste_aligne_dep:
-                        for case_aligne in alignement:
-                            if detect_moulin(case_aligne) == []:
-                                case_aligne.moulin = False #La case n'est plus alignée
-                                efface_moulin(case_aligne)
-                #Marque les cases si alignement
-                if liste_aligne_dest != []:
-                    retire = True
-                    dest.moulin = True
-                    dessine_moulin(dest)
-                    for alignement in liste_aligne_dest:
-                        for case_aligne in alignement:
-                            case_aligne.moulin = True #Marque la case comme alignée
-                            dessine_moulin(case_aligne)
-                else:
-                    joueur = joueur_adv(joueur)
-            else:
-                deplacement = False
-            efface("case_possible")
-            #Met fin au jeu si un des joueur gagne
-            if phase2 and (compteur["black"] == 7 or compteur["white"] == 7):
-                running = False
-                break
+            elif phase3[joueur] == True:
+                print("dans phase3")
+                depart = case
+                if depart.couleur == joueur:
+                    saut = True
+                    dessine_liste_cases(cases_vides(plateau))
 
-        #Le joueur sélectionne un pion à déplacer
-        elif phase2:
-            print("dans phase 2")
-            depart = donne_case()
-            if depart and depart.couleur == joueur:
-                deplacement = True
-                for case in depart.voisines:
-                    if case.couleur == "":
-                        dessine_liste_cases([case])
+            #Le joueur sélectionne un pion à déplacer
+            elif phase2:
+                print("dans phase2")
+                depart = case
+                if depart.couleur == joueur:
+                    deplacement = True
+                    for case in depart.voisines:
+                        if case.couleur == "":
+                            dessine_liste_cases([case])
+print("Le joueur", joueur_adv(joueur, j1, j2), "a gagné!")
 ferme_fenetre()
