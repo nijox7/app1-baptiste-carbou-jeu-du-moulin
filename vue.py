a45aa2591b68780a41a6f10e0e927d879d35decb
Clément <113636309+nijox7@users.noreply.github.com>
2023-01-06 08:36:03 +0100

    Update vue.py

--- vue.py
+++ vue.py
@@ -3,11 +3,10 @@ from fltk import*
 largeur, hauteur = 1700, 1000
 moyenne = (largeur + hauteur) / 1.75 #Moyenne qui permet d'ajuster la taille du quadrillage
 a = 0.25 * moyenne #Distance du centre au bord de la grille
-
+coeff = 1
 
 def dessine_plateau_9j():
     ''' Dessine un plateau pour 9 jetons '''
-
     rectangle(largeur/2 - a, hauteur/2 - a,
               largeur/2 + a, hauteur/2 + a, epaisseur=5)
     rectangle(largeur/2 - a * 2/3, hauteur/2 - a * 2/3,
@@ -25,10 +24,65 @@ def dessine_plateau_9j():
     ligne(largeur/2, hauteur/2 + a, largeur/2, hauteur/2 + a * 1/3,
           epaisseur=5)
 
+def dessine_plateau_12j():
+    ''' Dessine un plateau pour 12 jetons '''
+    dessine_plateau_9j()
+    #Ajoute les diagonales
+    ligne(largeur/2 - a, hauteur/2 - a,
+          largeur/2 - (1/3) * a, hauteur/2 - (1/3) * a,
+          epaisseur=5)
+    ligne(largeur/2 + a * (1/3), hauteur/2 + a * (1/3),
+          largeur/2 + a, hauteur/2 + a,
+          epaisseur=5)
+    ligne(largeur/2 - a, hauteur/2 + a,
+          largeur/2 - a * (1/3), hauteur/2 + a * (1/3),
+          epaisseur=5)
+    ligne(largeur/2 + a, hauteur/2 - a,
+          largeur/2 + a * (1/3), hauteur/2 - a * (1/3),
+          epaisseur=5)
+
+def dessine_plateau_6j():
+    ''' Dessine un plateau pour 6 jetons '''
+    rectangle(largeur/2 - a * 2/3, hauteur/2 - a * 2/3,
+              largeur/2 + a * 2/3, hauteur/2 + a * 2/3,
+              epaisseur=5)
+    rectangle(largeur/2 - a * 1/3, hauteur/2 - a * 1/3,
+              largeur/2 + a * 1/3, hauteur/2 + a * 1/3,
+              epaisseur=5)
+    ligne(largeur/2, hauteur/2 - a * (2/3),
+          largeur/2, hauteur/2 - a * (1/3),
+          epaisseur=5)
+    ligne(largeur/2, hauteur/2 + a * (2/3),
+          largeur/2, hauteur/2 + a * (1/3),
+          epaisseur=5)
+    ligne(largeur/2 + a * (2/3), hauteur/2,
+          largeur/2 + a * (1/3), hauteur/2,
+          epaisseur=5)
+    ligne(largeur/2 - a * (2/3), hauteur/2,
+          largeur/2 - a * (1/3), hauteur/2,
+          epaisseur=5)
+
+def dessine_plateau_3j():
+    rectangle(largeur/2 - a * 1/3, hauteur/2 - a * 1/3,
+              largeur/2 + a * 1/3, hauteur/2 + a * 1/3,
+              epaisseur=5)
+    ligne(largeur/2 - a * (1/3), hauteur/2 - a * (1/3),
+          largeur/2 + a * (1/3), hauteur/2 + a * (1/3),
+          epaisseur=5)
+    ligne(largeur/2 + a * (1/3), hauteur/2 - a * (1/3),
+          largeur/2 - a * (1/3), hauteur/2 + a * (1/3),
+          epaisseur=5)
+    ligne(largeur/2 - a * (1/3), hauteur/2,
+          largeur/2 + a * (1/3), hauteur/2,
+          epaisseur=5)
+    ligne(largeur/2, hauteur/2 - a * (1/3),
+          largeur/2, hauteur/2 + a * (1/3),
+          epaisseur=5)
+
 
 def dessine_pion(case):
     ''' Dessine un pion sur une case '''
-    cercle(largeur/2 - a + case.y * (a/3),  hauteur/2 - a + case.x * (a/3),
+    cercle(largeur/2 - a * coeff + case.y * (a/3),  hauteur/2 - a * coeff + case.x * (a/3),
            moyenne * 0.02, remplissage = case.couleur, couleur = case.couleur,
            tag="pion"+str(case.x)+str(case.y), epaisseur=2)
 
@@ -45,10 +99,11 @@ def distance (x, y):
 
 def donne_case(plateau):
     ''' Renvoie la case sur laquelle se trouve la souris, sinon None '''
-    for i in range(7):
-        for j in range(7):
-            x = hauteur/2 - a + i * (a/3)
-            y = largeur/2 - a + j * (a/3)
+    print(coeff)
+    for i in range(len(plateau)):
+        for j in range(len(plateau)):
+            x = hauteur/2 - a * coeff + i * (a/3)
+            y = largeur/2 - a * coeff + j * (a/3)
             if distance(x, y) <= moyenne * 0.02:
                 return plateau[i][j]
 
@@ -56,13 +111,13 @@ def donne_case(plateau):
 def dessine_liste_cases(liste):
     ''' Affiche une liste de case '''
     for case in liste:
-        cercle(largeur/2 - a + case.y * (a/3), hauteur/2 - a + case.x * (a/3),
+        cercle(largeur/2 - a * coeff + case.y * (a/3), hauteur/2 - a * coeff + case.x * (a/3),
                moyenne * 0.02, couleur="white", tag="case_possible")
 
 
 def dessine_moulin(case):
         ''' Marque une case '''
-        cercle(largeur/2 - a + case.y * (a/3), hauteur/2 - a + case.x * (a/3),
+        cercle(largeur/2 - a * coeff + case.y * (a/3), hauteur/2 - a * coeff + case.x * (a/3),
                moyenne * 0.02, couleur="yellow", epaisseur=3,
                tag="moulin"+str(case.x)+str(case.y))
 
@@ -78,6 +133,14 @@ def affiche_case(plateau):
         for j in range(len(plateau)):
             case = plateau[i][j]
             if case.couleur == "":
-                cercle(largeur/2 - a + case.y * (a/3), hauteur/2 - a + case.x * (a/3),
+                cercle(largeur/2 - a * coeff + case.y * (a/3), hauteur/2 - a * coeff + case.x * (a/3),
                    moyenne * 0.0075, couleur="black", remplissage="black",
                    tag="case")
+
+
+def touche_rectangle(coins):
+    ''' Renvoie si la souris touche le rectangle '''
+    if abscisse_souris() >= coins[0][0] and ordonnee_souris >= coins[0][1]:
+        if abscisse_souris() <= coins[1][0] and ordonnee_souris <= coins[1][1]:
+            return True
+    return False
